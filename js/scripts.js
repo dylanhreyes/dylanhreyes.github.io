ebdeded5765c16666611a4600c7b779a5547445c
Dylan Reyes <39449117+dylanhreyes@users.noreply.github.com>
2022-04-03 19:36:43 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-let images = ["cspresent.jpg", "cspresent2.jpeg", "cspresent3.jpeg", "mrdsax.jpg", "jazz.jpg","piano1.jpg", "piano2.jpg", "ptech.jpg", "ptech2.jpeg", "build.jpg"];
+let images = ["attheboard.jpg", "gtr.jpg", "headshot.jpeg", "piano1.jpg", "studiowork.JPG"];
 
 (() => {
   document.getElementById("main-bg").style =
-    `background-image: url('https://doongs.github.io/img/${images[Math.floor(Math.random() * images.length)]}'); height: 100vh`;
+    `background-image: url('https://dylanhreyes.github.io/img/${images[Math.floor(Math.random() * images.length)]}'); height: 100vh`;
 })();
